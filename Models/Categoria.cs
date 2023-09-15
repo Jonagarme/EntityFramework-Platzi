8fe3cf8621faeca938b1836467d341bb763a0d14
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-15 16:01:38 -0500

    conectado a sql server

---
+++ Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Categoria
+{
+    //[Key]
+    public Guid CategoriaId { get; set; }
+    //[Required]
+    //[MaxLength(150)]
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    
+    public virtual ICollection<Tarea> Tareas { get; set; }
+}
